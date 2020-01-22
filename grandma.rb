7c5d04ff04adbddf7286bc35114c92c88f37696e
Pedro David Garcia Lopez <reddevil_cero@hotmail.com>
2020-01-22 12:56:48 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -11,3 +11,13 @@
 
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
+def grandma(phrase)
+  if phrase != phrase.upcase 
+    return "HUH?! SPEAK UP, SONNY!"
+  elsif phrase == "I LOVE YOU GRANDMA!"
+    return "I LOVE YOU TOO PUMPKIN!"
+  else 
+    return "NO, NOT SINCE 1938!"
+  end
+end
+    
